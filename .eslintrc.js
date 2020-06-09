34d96a33b8cea6bd07e70ea5833bc7e42a897cc7
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-09 09:32:02 -0700

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
+    "ecmaVersion": 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "semi": ["error", "always"],
+    "no-unused-vars": "warn",
   },
   overrides: [
     {
